bd643f648f864bf2e5fd47e24ee1adc0cb28e3e3
justintrupiano <justintrupiano@gmail.com>
2019-06-05 12:39:36 -0600

    Single serial write per LED.

--- fiskeCube_sendImageSerial.py
+++ fiskeCube_sendImageSerial.py
@@ -5,7 +5,7 @@ import serial
 import cv2
 import struct
 import datetime
-
+import sys
 
 #####INITIALIZATION
 
@@ -26,19 +26,21 @@ imageList.append(cv2.imread("data/fiskeCube_AURORA.bmp"))
 # ser.port = serial.tools.list_ports.comports()[0]
 # ser.open()
 
-ser = serial.Serial('/dev/ttyACM0'); ## CHANGE TO WHATEVER PORT IS BEING USED
+ser = serial.Serial('/dev/ttyACM0') ## CHANGE TO WHATEVER PORT IS BEING USED
 ser.baudrate = 2000000
 
 
-
+s = str.encode('s') ### START READ VAR
+f = str.encode('f') ### SHOW LEDS VAR
+c = str.encode('c') ### CLEAR LEDS VAR
 #####Functions
 def sendFrame(thisFrame, currentImage):
 	framesWidth = currentImage.shape[1]
 	ledCount = 0
 	stripNum = 0
 
-	print('Frame started at ', datetime.datetime.now())
-	print('framesWidth: ', framesWidth)
+	# print('Frame started at ', datetime.datetime.now())
+	# print('framesWidth: ', framesWidth)
 	for y in range(0,framesWidth): ### FOR EACH PIXEL IN CURRENT FRAME
 		## OPENCV USES BGR FORMAT FOR COLORS
 		b = currentImage[thisFrame, y, 0]
@@ -48,29 +50,34 @@ def sendFrame(thisFrame, currentImage):
 		if(r==0 & g==0 & b==0):
 			pass
 		else:
-			ser.write(str.encode('s'))
-			ser.write(struct.pack('>B', r))
-			ser.write(struct.pack('>B', g))
-			ser.write(struct.pack('>B', b))
-			ser.write(struct.pack('>B', stripNum))
-			ser.write(struct.pack('>B', ledCount))
+			r = struct.pack('>B', r)
+			g = struct.pack('>B', g)
+			b = struct.pack('>B', b)
+			sN = struct.pack('>B', stripNum)
+			lC = struct.pack('>B', ledCount)
+
+			send = s + r + g + b + sN + lC
+			ser.write(send)
 
 		ledCount = ledCount + 1
 		if(ledCount == 256):
 			ledCount = 0
 			stripNum = stripNum + 1
 
-	print('Frame completed at ', datetime.datetime.now())
-	ser.write(str.encode('f'))
-	ser.write(str.encode('c'))
+	# print('Frame completed at ', datetime.datetime.now())
+	ser.write(f)
+	ser.write(c)
 
 
 def runCube():
 	for i in range(0, len(imageList)):				### IMAGE NUMBER < imageList LENGTH
+		# print('Viz number', i , 'started at', datetime.datetime.now())
+		sys.stdout.write('\rViz number ' + str(i) + ' started at ' + str(datetime.datetime.now()))
+
 		for j in range(0, (imageList[i].shape[0])):	### FRAME NUMBER < imageList[i] HEIGHT
 			sendFrame(j, imageList[i])
-			print('i: ', i)
-			print('j: ', j)
+			# print('i: ', i)
+			# print('j: ', j)
 
 
 
