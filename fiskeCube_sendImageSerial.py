a9daa24fa7e997812baf5e7a20171fc687031a5f
justintrupiano <justintrupiano@gmail.com>
2019-06-07 07:35:53 -0600

    Sends entire frame at once. Fixed & bug.

--- fiskeCube_sendImageSerial.py
+++ fiskeCube_sendImageSerial.py
@@ -1,14 +1,11 @@
 import serial
-# import serial.tools.list_ports
-# import glob ## TODO This is for autodetecting serial ports, but i'll work on that later
-# from PIL import Image
 import cv2
 import struct
 import datetime
-import os
+import sys, os
 
 dataDirName = os.path.dirname(__file__) + '/data/'
-# filename = os.path.join(dirname, './data/')
+
 #####INITIALIZATION
 
 
@@ -27,12 +24,13 @@ ser.baudrate = 2000000
 s = str.encode('s') ### START READ VAR
 f = str.encode('f') ### SHOW LEDS VAR
 c = str.encode('c') ### CLEAR LEDS VAR
+
 #####Functions
 def sendFrame(thisFrame, currentImage):
 	framesWidth = currentImage.shape[1]
 	ledCount = 0
 	stripNum = 0
-
+	sendPacket = struct.pack('>B', 0)
 	# print('Frame started at ', datetime.datetime.now())
 	# print('framesWidth: ', framesWidth)
 	for y in range(0,framesWidth): ### FOR EACH PIXEL IN CURRENT FRAME
@@ -41,7 +39,7 @@ def sendFrame(thisFrame, currentImage):
 		g = currentImage[thisFrame, y, 1]
 		r = currentImage[thisFrame, y, 2]
 
-		if(r==0 & g==0 & b==0):
+		if(r==0 and g==0 and b==0):
 			pass
 		else:
 			r = struct.pack('>B', r)
@@ -50,29 +48,25 @@ def sendFrame(thisFrame, currentImage):
 			sN = struct.pack('>B', stripNum)
 			lC = struct.pack('>B', ledCount)
 
-			send = s + r + g + b + sN + lC
-			ser.write(send)
+			sendPacket += s + r + g + b + sN + lC
 
 		ledCount = ledCount + 1
 		if(ledCount == 256):
 			ledCount = 0
 			stripNum = stripNum + 1
 
-	# print('Frame completed at ', datetime.datetime.now())
-	ser.write(f)
-	ser.write(c)
+	ser.write(sendPacket) 	#### SEND ONE ENTIRE FRAME OF BYTES
+	ser.write(f) 		  	#### SHOW LEDS
+	ser.write(c)		  	#### CLEAR LEDS
 
 
 def runCube():
 	for i in range(0, len(imageList)):				### IMAGE NUMBER < imageList LENGTH
 		# print('Viz number', i , 'started at', datetime.datetime.now())
-		print('Viz number ' + str(i) + ' started at ' + str(datetime.datetime.now()))
+		sys.stdout.write('\rViz number ' + str(i) + ' started at ' + str(datetime.datetime.now()))
 
 		for j in range(0, (imageList[i].shape[0])):	### FRAME NUMBER < imageList[i] HEIGHT
 			sendFrame(j, imageList[i])
-			# print('i: ', i)
-			# print('j: ', j)
-
 
 
 #### LOOP ####
@@ -81,4 +75,4 @@ while True:
 		runCube()
 
 	except ValueError as e:
-		print('Tell my debugger this context-relevant information: ', e)
+		print('Tell my debugger: ', e)
